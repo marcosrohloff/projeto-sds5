678578e34a4eca339b80f3112816b574bea6995e
marcosrohloff <marcos.fersh@gmail.com>
2021-11-03 11:55:13 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
+import DataTable from 'components/DataTable';
 
 function App() {
   return (
-    <div>
-      <h1>ola mundo</h1>
+    <>
+    <NavBar />
+    <div className="container">
+      <h1 className="text-primary">ola mundo</h1>
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
